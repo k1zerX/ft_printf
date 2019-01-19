2c98a35f66db8f9fbd9f00256edc4405def293ab
Kaleb Batz <kbatz@e3r6p2.21-school.ru>
2019-01-19 19:10:01 +0300

    printf

--- f_char.c
+++ f_char.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   f_char.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kbatz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/17 21:39:27 by kbatz             #+#    #+#             */
-/*   Updated: 2019/01/19 18:49:49 by kbatz            ###   ########.fr       */
+/*   Created: 2019/01/19 18:19:26 by kbatz             #+#    #+#             */
+/*   Updated: 2019/01/19 19:05:11 by kbatz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdarg.h>
-# include <libc.h>
-
-typedef struct	s_conv
+int		f_char(va_list ap, const char * restrict str, int len)
 {
-	char		conv;
-	int			(*f)(va_list ap, const char * restrict str, int len);
-}				t_conv;
+	int		c;
 
-int     f_char(va_list ap, const char * restrict str, int len);
-int     f(va_list ap, const char * restrict str, int len);
-
-#endif
+	c = va_arg(ap, int);
+	write(1, &c, 1);
+	return (len);
+}
--- ft_printf.c
+++ ft_printf.c
@@ -6,25 +6,73 @@
 /*   By: kbatz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 21:20:44 by kbatz             #+#    #+#             */
-/*   Updated: 2019/01/17 21:46:22 by kbatz            ###   ########.fr       */
+/*   Updated: 2019/01/19 19:04:13 by kbatz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-size_t		ft_printf(const char * restrict format, ...)
+t_conv			g_conv[] =
+{
+	{'c', &f_char},
+	/*{'s', &f},
+	{'p', &f},
+	{'d', &f},
+	{'i', &f},
+	{'o', &f},
+	{'u', &f},
+	{'x', &f},
+	{'X', &f},
+	{'f', &f},
+	{'%', &f},
+	{'e', &f},
+	{'g', &f},
+	{'b', &f},
+	{'r', &f},
+	{'k', &f},*/
+	{'\0', NULL}
+};
+
+int		ft_printf(const char *restrict str, ...)
 {
 	va_list		ap;
+	int			i;
+	int			len;
+	int			res;
 
-	va_start(ap, format);
+	va_start(ap, str);
 	while (*str)
 	{
-		if (*str == '%')
+		if (*str++ == '%')
 		{
-			va_arg(ap, <type>);
+			len = -1;
+			while (str[++len])
+			{
+				i = -1;
+				while (g_conv[++i].conv)
+				{
+					if (g_conv[i].conv == str[len])
+						res += g_conv[i].f(ap, str, len);
+					//printf("%c", str[len]);
+				}
+				if (!g_conv[i].conv)
+					break ;
+			}
+			str += len;
 		}
-		str++;
+		else
+			write(1, str - 1, 1);
 	}
 	va_end(ap);
+	return (res);
+}
+
+int		main(void)
+{
+	char	c;
+
+	c = 'Z';
+	ft_printf("blabla %c sdf f", c);
+	return (0);
 }
