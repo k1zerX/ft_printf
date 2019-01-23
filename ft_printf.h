d80352a80d39933e5ce418e0918ef2ef086e5d3b
Estell Tuffleberry <etuffleb@e1r4p4.21-school.ru>
2019-01-23 20:27:21 +0300

    ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: etuffleb <etuffleb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 21:39:27 by kbatz             #+#    #+#             */
-/*   Updated: 2019/01/23 16:20:13 by kbatz            ###   ########.fr       */
+/*   Updated: 2019/01/23 20:23:44 by etuffleb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,14 @@ int			ft_printf(const char *restrict str, ...);
 t_format	format(va_list ap, const char *restrict format, int len);
 int			ft_print(t_format f, char *str);
 
-char		*f_c(va_list ap);
-char		*f_s(va_list ap);
-char		*f_p(va_list ap);
-char		*f_di(va_list ap);
-char		*f_o(va_list ap);/*
-char		*f_u(va_list ap, const char *restrict str, int len);*/
-char		*f_x(va_list ap);
-char		*f_big_x(va_list ap);/*
+char		*f_c(va_list ap, t_format format);
+char		*f_s(va_list ap, t_format format);
+char		*f_p(va_list ap, t_format format);
+char		*f_di(va_list ap, t_format format);
+char		*f_o(va_list ap, t_format format);
+char		*f_u(va_list ap, t_format format);
+char		*f_x(va_list ap, t_format format);
+char		*f_big_x(va_list ap, t_format format);/*
 char		*f_f(va_list ap, const char *restrict str, int len);
 char		*f_e(va_list ap, const char *restrict str, int len);
 char		*f_g(va_list ap, const char *restrict str, int len);
