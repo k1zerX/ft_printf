df157fc2f53a34c29feda211c37e45a654e5979b
Estell Tuffleberry <etuffleb@at-r7.21-school.ru>
2019-08-04 16:32:59 +0300

    some norm

--- src/tools.c
+++ src/tools.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tools.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kbatz <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: etuffleb <etuffleb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/16 17:06:08 by kbatz             #+#    #+#             */
-/*   Updated: 2019/07/22 00:13:20 by kbatz            ###   ########.fr       */
+/*   Updated: 2019/08/04 16:20:51 by etuffleb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,30 +27,20 @@ int		itoa_len(unsigned long long int nbr, int base)
 	return (len);
 }
 
-char	*ft_ultra_itoa(long long int n, int base, char is_signed, const char *digits)
+void	ft_bzero(void *ptr, size_t size)
 {
-	char					*str;
-	int						len;
-	char					sign;
-	unsigned long long int	nbr;
+	while (size--)
+		*(unsigned char *)(ptr++) = 0;
+}
 
-	if (base < 2)
-		return (NULL);
-	if (is_signed && (sign = n < 0))
-		nbr = ~(unsigned long long int)n + 1;
-	else
-		nbr = n;
-//	printf("%llu %lld", nbr, n);
-	len = itoa_len(nbr, base) + 1;
-	if ((str = malloc((len + 1) * sizeof(*str))))
-	{
-		str[len] = 0;
-		str[0] = (sign) ? ('-') : ('+');
-		str[--len] = digits[nbr % base];
-		while ((nbr /= base) > 0)
-			str[--len] = digits[nbr % base];
-	}
-	return (str);
+int		ft_strlen(char *str)
+{
+	char	*start;
+
+	start = str;
+	while (*str)
+		++str;
+	return (str - start);
 }
 
 int		mini_atoi(const char *restrict *format, int *len)
@@ -63,106 +53,4 @@ int		mini_atoi(const char *restrict *format, int *len)
 	--(*format);
 	++(*len);
 	return (res);
-}
-
-void		ft_bzero(void *ptr, size_t size)
-{
-	while (size--)
-		*(unsigned char *)(ptr++) = 0;
-}
-
-int     ft_strlen(char *str)
-{
-	char	*start;
-	
-	start = str;
-	while (*str)
-		++str;
-	return (str - start);
-}
-
-void	ft_intfill(char *str, char *nbr, t_format f, int len)
-{
-	int		i;
-	int		j;
-
-	str[f.width + f.precision + len] = 0;
-	j = 0;
-	i = -1;
-	if (!f.minus && !f.zero)
-		while (++i < f.width)
-			str[j++] = ' ';
-	if (f.plus)
-		str[j++] = *nbr;
-	i = -1;
-	if (!f.minus && f.zero)
-		while (++i < f.width)
-			str[j++] = '0';
-	i = -1;
-	while (++i < f.precision)
-		str[j++] = '0';
-	i = -1;
-	while (++i < len)
-		str[j++] = *(++nbr);
-	i = -1;
-	if (f.minus)
-		while (++i < f.width)
-			str[j++] = ' ';
-}
-
-void	ft_strfill(char *str, char *s, t_format f, int len)
-{
-	int		i;
-	int		j;
-	char	c;
-
-	str[f.width + len] = 0;
-	c = (f.zero) ? ('0') : (' ');
-	j = 0;
-	i = -1;
-	if (!f.minus)
-		while (++i < f.width)
-			str[j++] = c;
-	i = -1;
-	while (++i < len)
-		str[j++] = *(s++);
-	i = -1;
-	if (f.minus)
-		while (++i < f.width)
-			str[j++] = c;
-}
-
-t_format	format(const char *restrict format, int len)
-{
-	t_format	f;
-
-	ft_bzero(&f, sizeof(f));
-	f.precision = -1;
-	while (--len >= 0)
-	{
-		if (*format == '#')
-			f.sharp = 1;
-		else if (*format == '.')
-		{
-			++format;
-			--len;
-			f.precision = mini_atoi(&format, &len);
-		}
-		else if (*format == '0')
-			f.zero = 1;
-		else if (ft_isdigit(*format))
-			f.width = mini_atoi(&format, &len);
-		else if (*format == '-')
-			f.minus = 1;
-		else if (*format == '+')
-			f.plus = 1;
-		else if (*format == ' ')
-			f.space = 1;
-		else if (*format == 'l')
-			++f.type;
-		else if (*format == 'h')
-			--f.type;
-		++format;
-	}
-	return (f);
 }
---
+++ src/tools2.c
@@ -0,0 +1,132 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etuffleb <etuffleb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/04 16:15:28 by etuffleb          #+#    #+#             */
+/*   Updated: 2019/08/04 16:30:36 by etuffleb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+char		*ft_ultra_itoa(long long int n, int base, char is_signed, \
+		const char *digits)
+{
+	char					*str;
+	int						len;
+	char					sign;
+	unsigned long long int	nbr;
+
+	if (base < 2)
+		return (NULL);
+	if (is_signed && (sign = n < 0))
+		nbr = ~(unsigned long long int)n + 1;
+	else
+		nbr = n;
+	len = itoa_len(nbr, base) + 1;
+	if ((str = malloc((len + 1) * sizeof(*str))))
+	{
+		str[len] = 0;
+		str[0] = (sign) ? ('-') : ('+');
+		str[--len] = digits[nbr % base];
+		while ((nbr /= base) > 0)
+			str[--len] = digits[nbr % base];
+	}
+	return (str);
+}
+
+void		ft_intfill(char *str, char *nbr, t_format f, int len)
+{
+	int		i;
+	int		j;
+
+	str[f.width + f.precision + len] = 0;
+	j = 0;
+	i = -1;
+	if (!f.minus && !f.zero)
+		while (++i < f.width)
+			str[j++] = ' ';
+	if (f.plus)
+		str[j++] = *nbr;
+	i = -1;
+	if (!f.minus && f.zero)
+		while (++i < f.width)
+			str[j++] = '0';
+	i = -1;
+	while (++i < f.precision)
+		str[j++] = '0';
+	i = -1;
+	while (++i < len)
+		str[j++] = *(++nbr);
+	i = -1;
+	if (f.minus)
+		while (++i < f.width)
+			str[j++] = ' ';
+}
+
+void		ft_strfill(char *str, char *s, t_format f, int len)
+{
+	int		i;
+	int		j;
+	char	c;
+
+	str[f.width + len] = 0;
+	c = (f.zero) ? ('0') : (' ');
+	j = 0;
+	i = -1;
+	if (!f.minus)
+		while (++i < f.width)
+			str[j++] = c;
+	i = -1;
+	while (++i < len)
+		str[j++] = *(s++);
+	i = -1;
+	if (f.minus)
+		while (++i < f.width)
+			str[j++] = c;
+}
+
+void		reformat(t_format *f)
+{
+	if (*format == '-')
+		f->minus = 1;
+	else if (*format == '+')
+		f->plus = 1;
+	else if (*format == ' ')
+		f->space = 1;
+	else if (*format == 'l')
+		++(f->type);
+	else if (*format == 'h')
+		--(f->type);
+}
+
+t_format	format(const char *restrict format, int len)
+{
+	t_format	f;
+
+	ft_bzero(&f, sizeof(f));
+	f.precision = -1;
+	while (--len >= 0)
+	{
+		if (*format == '#')
+			f.sharp = 1;
+		else if (*format == '.')
+		{
+			++format;
+			--len;
+			f.precision = mini_atoi(&format, &len);
+		}
+		else if (*format == '0')
+			f.zero = 1;
+		else if (ft_isdigit(*format))
+			f.width = mini_atoi(&format, &len);
+		else if (*format == '-' || *format == '+' || *format == ' ' || \
+				*format == 'l' || *format == 'h')
+			reformat(&f);
+		++format;
+	}
+	return (f);
+}
