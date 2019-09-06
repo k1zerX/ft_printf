85f586fc3c7105b148bc96e60e313b08f9bfebee
Kaleb Batz <kbatz@il-d1.21-school.ru>
2019-09-07 00:20:05 +0300

    segfault fixed

--- src/tools3.c
+++ src/tools3.c
@@ -6,11 +6,41 @@
 /*   By: kbatz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/04 19:23:48 by kbatz             #+#    #+#             */
-/*   Updated: 2019/08/15 23:04:24 by kbatz            ###   ########.fr       */
+/*   Updated: 2019/09/07 00:19:22 by kbatz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	print_byte(unsigned char byte)
+{
+	int		i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		if (byte & (1 << i))
+			write(1, "1", 1);
+		else
+			write(1, "0", 1);
+		--i;
+	}
+}
+
+void	print_bits(void *ptr, size_t size)
+{
+	unsigned char	*tmp;
+
+	tmp = (unsigned char *)ptr + size;
+	print_byte(*--tmp);
+	while (--size)
+	{
+		write(1, " ", 1);
+		print_byte(*--tmp);
+	}
+	write(1, "\n", 1);
+}
+#include <stdio.h>
 
 void	apa_fill_i(char *str, unsigned long m, int len, int k)
 {
@@ -41,23 +71,35 @@ void	apa_fill_f(char *str, unsigned long m, int len, int k)
 	int		i;
 	int		l;
 
+//	printf("%d %d\n", len, k);
 	i = -1;
-	while (++i < len)
+	while (++i < len + k)
 		str[i] = 0;
 	l = 1;
 	i = -1;
 	while (++i < len)
 	{
-		if (m & ((unsigned long)1 << i))
-			str[len - 1 - i] += 1;
-		apa_mul(5, str, len, &l);
+//		print_bits(&m, sizeof(m));
+		if (m & ((unsigned long)1))
+			str[len + k - 1 - i] += 1;
+		m >>= 1;
+		apa_mul(5, str, len + k, &l);
+/*		int j = -1;
+		while (++j < len)
+			str[j] += '0';
+		write(1, str, len);
+		write(1, "\n", 1);
+		j = -1;
+		while (++j < len)
+			str[j] -= '0';*/
 	}
 	i = -1;
 	while (++i < k)
-		apa_mul(5, str, len, &l);
+		apa_mul(5, str, len + k, &l);
 	i = -1;
-	while (++i < len)
+	while (++i < len + k)
 		str[i] += '0';
+//		print_bits(&m, sizeof(m));
 }
 
 void	apa_if(int *k, t_float f, t_float_ip *parts)
@@ -70,21 +112,24 @@ void	apa_if(int *k, t_float f, t_float_ip *parts)
 		parts->f_len = 0;
 		*k -= 52;
 	}
-	else if (k < 0)
+	else if (*k < 0)
 	{
 		parts->i = 0;
 		parts->i_len = 0;
 		parts->f_len = count_len(f.parse.m);
-		parts->f = (f.parse.m >> (52 - parts->i_len)) | \
-				((unsigned long)1 << parts->i_len);
-		*k += ++parts->f_len;
+		parts->f = (f.parse.m >> (52 - parts->f_len)) | \
+				((unsigned long)1 << parts->f_len);
+		++parts->f_len;
 	}
 	else
 	{
 		parts->i = (f.parse.m >> (52 - *k)) | ((unsigned long)1 << (*k));
 		parts->i_len = *k + 1;
-		parts->f_len = count_len(f.parse.m) - *k;
-		parts->f = (f.parse.m << *k) >> (52 - parts->f_len);
+		parts->f_len = count_len(f.parse.m);
+		parts->f = f.parse.m >> (52 - parts->f_len);
+		parts->f_len -= *k;
+		parts->f &= ~(~((unsigned long)0) << parts->f_len);
+//		printf("%lu\n", parts->f);
 		*k = 0;
 	}
 }
@@ -93,13 +138,14 @@ char	*apa_get(t_float f, t_float_ip parts, int il, int k)
 {
 	char	*str;
 
-	if (!(str = malloc((1 + il + 1 + parts.f_len + 1) * sizeof(char))))
+	if (!(str = malloc((1 + il + 1 + parts.f_len + ((k > 0) ? (0) : (-++k)) + 1) * sizeof(char))))
 		exit(1);
+//	printf("1 + %d + 1 + %d + 1\n", il, parts.f_len + ((k > 0) ? (0) : (-k)));
 	str[0] = (f.parse.s) ? '-' : '+';
 	apa_fill_i(str + 1, parts.i, il, k);
 	str[1 + il] = '.';
-	apa_fill_f(str + 1 + il + 1, parts.f, parts.f_len, k);
-	str[1 + il + 1 + parts.f_len] = 0;
+	apa_fill_f(str + 1 + il + 1, parts.f, parts.f_len, -k);
+	str[1 + il + 1 + parts.f_len + (-k)] = 0;
 	return (str);
 }
 
@@ -122,6 +168,7 @@ char	*apa_float(double n)
 	}
 	k = f.parse.e - 1023;
 	apa_if(&k, f, &parts);
-	il = (int)((parts.i_len + ((k < 0) ? (0) : (k))) * 0.30103) + 1;
+	il = (int)((((parts.i_len) ? (parts.i_len) : (1)) + ((k < 0) ? (0) : (k))) * 0.30103) + 1;
+	//printf("(%d - 1 + %d) * 0.30103 + 1 = %d\n", parts.i_len, k, il);
 	return (apa_get(f, parts, il, k));
 }
--- src/tools4.c
+++ src/tools4.c
@@ -6,7 +6,7 @@
 /*   By: kbatz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/04 19:44:42 by kbatz             #+#    #+#             */
-/*   Updated: 2019/08/04 19:46:24 by kbatz            ###   ########.fr       */
+/*   Updated: 2019/09/07 00:17:47 by kbatz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,17 @@ int		count_len(unsigned long a)
 	return (52 - i);
 }
 
+#include <stdio.h>
+
 void	apa_mul(int power, char *str, int len, int *l)
 {
 	int		i;
 	int		buf;
 
+//	printf("%d\n", len);
 	buf = 0;
 	i = len;
-	while (--i >= 0 && len - i <= *l)
+	while (--i >= 0 /*&& len - i <= *l*/)
 	{
 		str[i] *= power;
 		str[i] += buf;
@@ -38,5 +41,7 @@ void	apa_mul(int power, char *str, int len, int *l)
 		str[i] -= buf * 10;
 		if ((len - i == *l) && buf)
 			++*l;
+//		printf("%d", str[i]);
 	}
+//	printf("\n");
 }
